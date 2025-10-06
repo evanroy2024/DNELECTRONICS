a71d24a9cf306f48cd2ea299b1e964d155979f5c
erenroy <roypapan75@gmail.com>
2025-10-06 17:57:54 +0530

    add
    add

--- dnelectronics/dnelectronics/settings.py
+++ dnelectronics/dnelectronics/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'dnelectronics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dnelectronics_db',
+        'USER': 'dnelectronics_user',
+        'PASSWORD': 'DnElec2024#Secure',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
